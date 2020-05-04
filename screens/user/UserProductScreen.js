84d25db8a912be428d98c5c9589a7a9751873384
Bruni12 <brunnetteboy@gmail.com>
2020-05-04 13:36:01 -0600

    handling user inputs

--- screens/user/UserProductScreen.js
+++ screens/user/UserProductScreen.js
@@ -59,15 +59,17 @@ UserProductsScreen.navigationOptions = navData => {
         />
       </HeaderButtons>
     ),
-    headerRight:  <HeaderButtons HeaderButtonComponent={HeaderButton}>
-    <Item
-      title="Add"
-      iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
-      onPress={() => {
-        navData.navigation.navigate('EditProduct');
-      }}
-    />
-  </HeaderButtons>
+    headerRight:  (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add"
+          iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+          onPress={() => {
+            navData.navigation.navigate('EditProduct');
+          }}
+      />
+    </HeaderButtons>
+    )
   };
 };
 
