2c851fa4db69b75595693721fab28137c866a26d
Bruni12 <brunnetteboy@gmail.com>
2020-05-09 14:22:00 -0600

    updated product information

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,5 +1,33 @@
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const CREATE_PRODUCT = 'CREATE_PRODUCT';
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
 }; 
+
+export const CreateProduct = (title, description, imageUrl, price) => {
+  return { 
+    type: CREATE_PRODUCT, 
+    productData: {
+      title,
+      description: 
+      description,
+      imageUrl,
+      price
+    }
+  };
+};
+
+export const CreateProduct = (id, title, description, imageUrl ) => {
+  return { 
+    type: CREATE_PRODUCT, 
+    pid: id,
+    productData: {
+      title,
+      description: 
+      description,
+      imageUrl
+    }
+  };
+};
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,5 +1,5 @@
 import PRODUCTS from '../../data/dummy-data';
-import { DELETE_PRODUCT } from '../actions/products';
+import { DELETE_PRODUCT, CREATE_PRODUCT, UPDATE_PRODUCT } from '../actions/products';
 
 const initialState = {
   availableProducts: PRODUCTS,
@@ -8,7 +8,33 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case DELETE_PRODUCT:
+      case CREATE_PRODUCT: 
+        const newProduct = new Product 
+        (new Date().toString(), 
+        'ul',
+        action.prodcutData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        action.prodcutData.price
+       );
+       return {
+         ...state,
+         availableProducts: state.availableProducts.concat(newProduct),
+         userProducts: state.userProducts.concat(newProduct)
+       };
+     case UPDATE_PRODUCT:
+          const productIndex = state.userProducts.findIndex(
+            prod => prod.id === action.pid
+          );
+          const updateProduct = new Product(
+          action.pid,
+          state.userProducts[productIndex].ownerId,
+          action.productData.title,
+          action.productData.imageUrl,
+          action.productData.description,
+          state.userProducts[productIndex].price,
+        );
+     case DELETE_PRODUCT:
       return {
         ...state,
         userProducts: state.userProducts.filter(
